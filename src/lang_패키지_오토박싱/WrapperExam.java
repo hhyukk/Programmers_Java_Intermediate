8f402c873b002a0e7158ce7be5d3973a8e20583b
hhyukk <hhyukk1273@gmail.com>
2025-11-07 11:12:34 +0900

    java.lang 패키지/오토박싱 자바는 기본적으로 다양한 패키지를 지원하는데 그중에서 가장 중요한 패키지다.

---
+++ src/lang_패키지_오토박싱/WrapperExam.java
@@ -0,0 +1,17 @@
+package lang_패키지_오토박싱;
+
+public class WrapperExam {
+    public static void main(String[] args) {
+        int i = 5;
+
+//        오토박싱 기능이 생기면서 Java 16 이후에는 제거된 생성자
+//        Integer i2 = new Integer(5);
+
+//        내부적으로 Integer i3 = new Integer(5);과 같음
+        Integer i3 = 5;
+
+        int i4 = i3.intValue();
+
+        int i5 = i3;
+    }
+}
