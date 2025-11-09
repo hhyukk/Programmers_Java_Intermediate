2201bfd9e74fea3abc6523f31f52e60105914f1d
hhyukk <hhyukk1273@gmail.com>
2025-11-09 12:47:38 +0900

    Char 단위 입출력(File)

---
+++ src/IO/CharIOExam02.java
@@ -0,0 +1,27 @@
+package IO;
+
+import java.io.*;
+
+public class CharIOExam02 {
+    public static void main(String[] args) {
+        BufferedReader br = null;
+        PrintWriter pw = null;
+        try {
+            br = new BufferedReader(new FileReader("src/IO/CharIOExam02.java"));
+            pw = new PrintWriter(new FileWriter("test.txt"));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                pw.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pw.close();
+            try {
+                br.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
